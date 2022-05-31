304aa852d0240880fe26ca824fcc35b7616ddcae
Tran Nhut <tnhut803@gmail.com>
2022-05-31 14:08:28 +0700

    fix schema

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const { route } = require("express/lib/application");
 const router = express.Router();
-
+router.get("/cach-de-phong-iphone-bi-nhiem-virus", (req, res) => {
+  res.render("blogThree", { error: true });
+});
 router.get("/iphone14-lieu-con-dinh-loi-nguyen-ve-pin", (req, res) => {
-  res.render("blog-1", { error: true });
+  res.render("blogOne", { error: true });
 });
 router.get("/4-cach-thiet-lap-kiem-soat-phu-huynh-cho-safari", (req, res) => {
   res.render("blog-2");
+});
+router.get("/", (req, res) => {
+  res.render("blogs");
 });
 module.exports = router;
